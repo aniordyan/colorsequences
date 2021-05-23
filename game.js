e4363bd49f4b771c531f8a85bcaf74546a233282
aniordyan <77895035+aniordyan@users.noreply.github.com>
2021-05-23 18:08:59 +0400

    Update game.js

--- game.js
+++ game.js
@@ -57,7 +57,7 @@ function checkAnswer(currentLevel) {
       setTimeout(function () { $("body").removeClass("game-over")
 
       }, 200);
-      $("h1").text("Սխալվեցիր: Վերսկսելու համար սեղմել ցանկացած կոջակ");
+      $("h1").text("Սխալվեցիր: Վերսկսելու համար սեղմել ցանկացած կոճակ");
       startOver();
   }
 }
